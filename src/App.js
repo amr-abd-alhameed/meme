79cca462095513025e1d893abd4178a38eb062bd
amr <aa5473@fayoum.edu.eg>
2023-02-04 22:04:19 +0300

    my name is amr

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import "./App.scss";
+import "normalize-scss";
+import {
+    Header,
+    Home,
+    About,
+    Exp,
+    Projects,
+    Contact,
+    HI,
+    Footer,
+} from "./components/index";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./components/common/index";
+
+function App() {
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <Router>
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        {/* <Route path="/Exp" element={<Exp />} /> */}
+                    </Routes>
+                    <About />
+                    <Exp />
+                    <Projects />
+                    <Contact />
+                    <HI />
+                    <Footer />
+                </Router>
+            </ThemeProvider>
+        </>
+    );
+}
+
+export default App;
